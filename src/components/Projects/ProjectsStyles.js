de5990451da318482d533c36e65c7dbaa1d29eb2
Yasser Shkeir <shkeiryasser@gmail.com>
2022-07-06 16:21:11 +0300

    edit img

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Img = styled.img`
-  vertical-align: middle
+  vertical-align: middle;
   height: 100px;
   object-fit: cover;
   overflow: hidden;
