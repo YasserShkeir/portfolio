25386be3ed4aa83cb5b3c76ddf25658be349e313
Yasser Shkeir <shkeiryasser@gmail.com>
2022-07-06 16:05:11 +0300

    edit projects 2

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -22,11 +22,11 @@ export const GridContainer = styled.section`
 `;
 
 export const BlogCard = styled.div`
-  border-radius: 10px;
+  border-radius: 20px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
   width: 400px;
-  height: 600px;
+  height: 500px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
